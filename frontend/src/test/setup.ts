4b365bd78d2c1f3126f21c53e2c4e8bea33c40c7
June3141 <16346377+June3141@users.noreply.github.com>
2026-02-19 03:14:52 +0000

    test: ✅ add MSW server and test data factories

--- frontend/src/test/setup.ts
+++ frontend/src/test/setup.ts
@@ -1,13 +1,17 @@
 import '@testing-library/jest-dom/vitest';
-import { afterEach } from 'vitest';
+import { afterAll, afterEach, beforeAll } from 'vitest';
 
 import { useAgentStore } from '../stores/agentStore';
 import { useAuthStore } from '../stores/authStore';
 import { useBoardStore } from '../stores/boardStore';
 import { useToastStore } from '../stores/toastStore';
 import { useUiStore } from '../stores/uiStore';
+import { server } from './mocks/server';
+
+beforeAll(() => server.listen({ onUnhandledRequest: 'warn' }));
 
 afterEach(() => {
+  server.resetHandlers();
   useAgentStore.setState(useAgentStore.getInitialState());
   useAuthStore.setState(useAuthStore.getInitialState());
   useBoardStore.setState(useBoardStore.getInitialState());
@@ -15,3 +19,5 @@ afterEach(() => {
   useUiStore.setState(useUiStore.getInitialState());
   sessionStorage.clear();
 });
+
+afterAll(() => server.close());
