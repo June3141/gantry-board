0b891d1ceeaed18306a8733b0cf2cc73583f5565
June3141 <16346377+June3141@users.noreply.github.com>
2026-03-01 02:56:45 +0000

    fix: 🐛 use absolute path in ENTRYPOINT and detect lockfile changes

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-# Install dependencies if node_modules is missing or package.json has changed
-if [ ! -d node_modules ] || [ package.json -nt node_modules/.package-lock.json ]; then
+# Install dependencies if node_modules is missing or package manifest/lockfile has changed
+if [ ! -d node_modules ] || [ package.json -nt node_modules/.package-lock.json ] || [ package-lock.json -nt node_modules/.package-lock.json ]; then
   echo "Installing dependencies..."
   npm ci
 fi
